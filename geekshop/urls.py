10dd351a051a5818c6d8a0759ff5bb78f60dab94
majahed228 <pluzhnikov93@inbox.ru>
2022-02-08 21:17:01 +0300

    added def text_context

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from products.views import index, products
+from products.views import index, products, text_context
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('products/', products, name='products'),
+    path('text-context/', text_context, name='text_context'),
 ]
