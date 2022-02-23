12d5287fa2a07b16d52ba0eb2ade8239a559a379
majahed228 <pluzhnikov93@inbox.ru>
2022-02-23 23:21:15 +0300

    added UserProfileForm

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.contrib import auth
 from django.urls import reverse
 from django.contrib import messages
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 
 def login(request):
@@ -38,7 +38,8 @@ def registration(request):
 
 
 def profile(request):
-    context = {'title': 'GeekShop - Профиль'}
+    form = UserProfileForm(instance=request.user)
+    context = {'title': 'GeekShop - Профиль', 'form': form}
     return render(request, 'users/profile.html', context)
 
 
