fabd7dae56fee07d61c21a0e7575f2b7c12f1bf8
majahed228 <pluzhnikov93@inbox.ru>
2022-03-02 20:11:26 +0300

    added controller admin_users_delete()

--- admins/urls.py
+++ admins/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from admins.views import index, admin_users, admin_users_create, admin_users_update
+from admins.views import index, admin_users, admin_users_create, admin_users_update, admin_users_delete
 
 app_name = 'admins'
 
@@ -8,5 +8,6 @@ urlpatterns = [
      path('', index, name='index'),
      path('users/', admin_users, name='admin_users'),
      path('users-create/', admin_users_create, name='admin_users_create'),
-     path('users-update/<int:pk>/', admin_users_update, name='admin_users_update')
+     path('users-update/<int:pk>/', admin_users_update, name='admin_users_update'),
+     path('users-delete/<int:pk>/', admin_users_delete, name='admin_users_delete')
 ]
--- admins/views.py
+++ admins/views.py
@@ -44,3 +44,10 @@ def admin_users_update(request, pk):
         form = UserAdminProfileForm(instance=selected_user)
     context = {'title': 'GeekShop - Admin', 'form': form, 'selected_user': selected_user}
     return render(request, 'admins/admin-users-update-delete.html', context)
+
+
+# Delete controller
+def admin_users_delete(request, pk):
+    user = User.objects.get(id=pk)
+    user.safe_delete()
+    return HttpResponseRedirect(reverse('admin_staff:admin_users'))
