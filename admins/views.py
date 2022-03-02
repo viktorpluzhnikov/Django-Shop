9d3a391d89ae6834b94a87c09975c5562228562f
majahed228 <pluzhnikov93@inbox.ru>
2022-03-02 18:21:24 +0300

    added controller admin_users()

--- admins/views.py
+++ admins/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from users.models import User
+
 
 def index(request):
-    context = {'title': 'GeekShop - Админ-панель'}
+    context = {'title': 'GeekShop - Admin'}
     return render(request, 'admins/index.html', context)
+
+
+# Read controller
+def admin_users(request):
+    users = User.objects.all
+    context = {'title': 'GeekShop - Admin', 'users': users}
+    return render(request, 'admins/admin-users-read.html', context)
