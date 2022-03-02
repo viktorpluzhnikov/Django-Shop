3d78a39a1eb28608c2903ffc995bbfe0c8dd18c4
majahed228 <pluzhnikov93@inbox.ru>
2022-03-02 20:33:55 +0300

    added ProductAdmin class

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,12 @@ from django.contrib import admin
 from products.models import Product, ProductCategory
 
 
-admin.site.register(Product)
 admin.site.register(ProductCategory)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'image', 'description', 'price', 'quantity', 'category')
+    search_fields = ('name',)
+    ordering = ('name',)
