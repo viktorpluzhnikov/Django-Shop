12ac17dd0df516717e7ce172f5595e4fa049dd32
majahed228 <pluzhnikov93@inbox.ru>
2022-03-07 13:59:02 +0300

    added paginator

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.conf import settings
 
 from products.models import Product, ProductCategory
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def index(request):
@@ -10,14 +11,22 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request, category_id=None):
+def products(request, category_id=None, page=1):
     if category_id:
         products = Product.objects.filter(category_id=category_id)
     else:
         products = Product.objects.all()
+
+    paginator = Paginator(products, 3)
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
     context = {'title': 'GeekShop - Продукты',
                'categories': ProductCategory.objects.all(),
-               'products': products
+               'products': products_paginator,
                }
     return render(request, 'products/products.html', context)
 
